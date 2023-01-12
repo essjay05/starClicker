873cdd6de4b947f4de89d5b8f44d4b946a118bf4
Joy Serquiña <stepjoyous@gmail.com>
2023-01-11 23:44:10 -0800

    Removed unused code and returned logic to previous state.

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-// function update() {
-//   document.getElementById('text').value = cookiecount;
-//   document.title = cookiecount + " Cookies";
-  
-//   document.getElementById('ammountMultiplier').innerHTML = "Multiplier Upgrade x" + (multiplier+1);
-//   document.getElementById('ammountMultiplier2').innerHTML = "x" + (multiplier+1);
-//   document.getElementById('costMultiplier').innerHTML = ((multiplier+1) * 100) + " Cookies";
-//   document.getElementById('currentMultiplier').innerHTML = "Your current Multiplier is x" + (multiplier);
-  
-//   document.getElementById('ammountAutoClick').innerHTML = "You Own " + autoClick + " Auto Clickers";
-//   document.getElementById('costAutoClick').innerHTML = ((autoClick+1) * 12) + " Cookies";
-  
-//   document.getElementById('ammountFarms').innerHTML = "You Own " + farms + " Farms";
-//   document.getElementById('costFarms').innerHTML = ((farms+1) * 15) + " Cookies";
-  
-//   document.getElementById('cookiespersecond').innerHTML = "You are gaining " + (((autoClick)+(farms*2))*multiplier) + " Cookies per/s";
-// }
-
 // Set starting score to 0
 var score = 0;
 var multiplier = 1;
 var mushrooms = 0;
 var greenMushrooms = 0;
-
 
 
 // Function to add one per click
@@ -34,15 +15,13 @@ function generateStar() {
   scoreboard.innerHTML = score;
 }
 
-// Function to for upgrading with mushrooms
+// Function to for upgrading to Mario
 function addMushroom() {
-  // Select set scoreboard variable to starCount element
-  var scoreboard = document.getElementById("starCount");
-
   if ( score >= 30 ) {
     score -= 30;
     mushrooms +=1;
-    
+    // Select set scoreboard variable to starCount element
+    var scoreboard = document.getElementById("starCount");
     // Change score on html with each click
     scoreboard.innerHTML = score;
     // Select set scoreboard variable to starCount element
@@ -53,15 +32,15 @@ function addMushroom() {
   } else {
     alert('Not enough stars yet to upgrade!')
   }
-  
+
   function timer() {
   score = score + mushrooms;
   score = score + greenMushrooms;
-    // // Select set scoreboard variable to starCount element
-    // var scoreboard = document.getElementById("starCount");
+    // Select set scoreboard variable to starCount element
+    var scoreboard = document.getElementById("starCount");
     // Change score on html with each click
     scoreboard.innerHTML = score;
   }
   setInterval(timer, 1000);
-  
+
 }
